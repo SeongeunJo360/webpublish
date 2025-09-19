cd316f05151e2214f4fe47e91abff4883c04d9e7
ingeng1080-cell <ingeng1080@gmail.com>
2025-09-19 16:54:01 +0900

    25-09-19

--- react/shoppy-app/src/App.js
+++ react/shoppy-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './pages/Layout.jsx';
+import { Products } from './pages/Products.jsx';
+import { Home } from './pages/Home.jsx';
+import { Login } from './pages/Login.jsx';
+import { Signup } from './pages/Signup.jsx';
+import { ProductDetail } from './pages/ProductDetail.jsx';
+import { Cart } from './pages/Cart.jsx';
+import './styles/cgvSignup.css';
+import './styles/cgv.css';
+import './styles/shoppy.css';
 
-function App() {
+export default function App() {
+  //1. 장바구니 수량 관리 : setCartCount
+  const [cartCount, setCartCount] = useState(0);
+
+  //2. 장바구니 아이템 관리
+  
+  const addCart = (number) => {
+    // console.log(number);
+    setCartCount(cartCount + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout cartCount={cartCount} />}>
+          <Route index element={<Home/>} />
+          <Route path="/all" element={<Products/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/products/:pid" element={<ProductDetail addCart={addCart} />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
-export default App;
+
+
+
+
+
