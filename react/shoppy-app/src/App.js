9a0c4ead6905890c4c89d185b4751328dbb58f05
ingeng1080-cell <ingeng1080@gmail.com>
2025-09-23 18:23:09 +0900

    25-09-23

--- react/shoppy-app/src/App.js
+++ react/shoppy-app/src/App.js
@@ -7,20 +7,42 @@ import { Login } from './pages/Login.jsx';
 import { Signup } from './pages/Signup.jsx';
 import { ProductDetail } from './pages/ProductDetail.jsx';
 import { Cart } from './pages/Cart.jsx';
+
+import { cartItemsCheck, updateCartItemsQty } from './utils/cart.js';
+
 import './styles/cgvSignup.css';
 import './styles/cgv.css';
 import './styles/shoppy.css';
 
 export default function App() {
-  //1. 장바구니 수량 관리 : setCartCount
-  const [cartCount, setCartCount] = useState(0);
-
-  //2. 장바구니 아이템 관리
+  const [cartCount, setCartCount] = useState(0);  //1. 장바구니 수량 관리  
+  const [cartItems, setCartItems] = useState([]); //2. 장바구니 아이템 관리
   
-  const addCart = (number) => {
-    // console.log(number);
+  const addCart = (cartItem) => {  // <-- ProductDetail 쇼핑백 추가 이벤트 처리
+    setCartItems(cartItemsCheck(cartItems, cartItem));
     setCartCount(cartCount + 1);
   }
+
+  const updateCart = (cid, type) => {
+    // console.log(cid, type);
+    if(type === undefined) {
+        const findItem = cartItems.find(item => item.cid === cid);
+        setCartCount(cartCount - findItem.qty);
+
+        setCartItems((cartItems) => {
+            return cartItems.filter(item => !(item.cid === cid));  
+        });
+
+    } else {
+      setCartItems(updateCartItemsQty(cartItems, cid, type));
+      type === "+" ? setCartCount(cartCount + 1) 
+                : cartCount > 1 ? setCartCount(cartCount - 1) : setCartCount(cartCount);
+    }
+  }
+
+  console.log('cartItems-->', cartItems);
+  
+  
 
   return (
     <BrowserRouter>
@@ -30,17 +52,11 @@ export default function App() {
           <Route path="/all" element={<Products/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<Signup/>} />
-          <Route path="/cart" element={<Cart/>} />
+          <Route path="/cart" element={<Cart  items={cartItems} 
+                                              updateCart={updateCart}/>} />
           <Route path="/products/:pid" element={<ProductDetail addCart={addCart} />} />
         </Route>
       </Routes>
     </BrowserRouter>
-
   );
 }
-
-
-
-
-
-
